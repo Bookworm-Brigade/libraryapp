0bcf01105179c11d2765eb8cbd797d0654c7c9de
Pearl Sarpong <pearlsensory01@gmail.com>
2025-03-11 09:09:40 +0000

    books page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar text-xl flex justify-end'>
+    <p>All Books</p>
+        <p>Categories</p>
+        <IoIosAddCircleOutline /><p>Add Books</p>
+    </div>
+
+
   )
 }
